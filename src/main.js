5890cb528d532d336dee4bb93dd313ecc2d969e4
benbarrios87 <benbarrios87@gmail.com>
2026-07-13 08:13:44 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import "./styles/index.css";
+import { router } from "./router/router.js";
+import { AppShell } from "./components/layout/app-shell.js";
+import { showFatalError } from "./components/ui/error-state.js";
+
+async function start() {
+  const app = document.querySelector("#app");
+
+  if (!app) {
+    throw new Error("Fant ikke #app.");
+  }
+
+  app.innerHTML = AppShell();
+  await router.start();
+}
+
+start().catch((error) => {
+  console.error(error);
+  showFatalError(error);
+});
